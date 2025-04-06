ef34f13265ce7a75fe3b16889df6f1bb2d1e92b2
AntiElitz <36789897+AntiElitz@users.noreply.github.com>
2025-04-06 05:16:42 +0200

    Update config.js

--- Factorio Preview Viewer/config.js
+++ Factorio Preview Viewer/config.js
@@ -1,7 +1,7 @@
 const planetConfig = {
-  Nauvis: "../previews/nauvis.png",
-  Vulcanus: "../previews/vulcanus.png",
-  Gleba: "../previews/gleba.png",
-  Fulgora: "../previews/fulgora.png",
-  Aquilo: "../previews/aquilo.png"
+  Nauvis: "https://drive.google.com/uc?export=view&id=1I0uSOoZHozGAqkuSVhBWtYnWijcFCoJK",
+  Vulcanus: "https://drive.google.com/uc?export=view&id=1Po6cfU9tbBQT97UL6x2FKWAV5IaamVcv",
+  Gleba: "https://drive.google.com/uc?export=view&id=1VeEVNVGYSDa_l3T_NQZKa054APTqtCYM",
+  Fulgora: "https://drive.google.com/uc?export=view&id=1tygQp-aaHHxehmT2HgdVxPv12CWBghBl",
+  Aquilo: "https://drive.google.com/uc?export=view&id=1tTYtliSC3y_5Le0S1iZmVEsQz7_CRSRj"
 };
