e15c81c9d8f67041dff5a44f6f7fecf8bf3b66dc
AntiElitz <36789897+AntiElitz@users.noreply.github.com>
2025-04-08 02:48:51 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const planetConfig = {
-  Nauvis: "https://drive.google.com/uc?export=view&id=1I0uSOoZHozGAqkuSVhBWtYnWijcFCoJK",
-  Vulcanus: "https://drive.google.com/uc?export=view&id=1Po6cfU9tbBQT97UL6x2FKWAV5IaamVcv",
-  Gleba: "https://drive.google.com/uc?export=view&id=1VeEVNVGYSDa_l3T_NQZKa054APTqtCYM",
-  Fulgora: "https://drive.google.com/uc?export=view&id=1tygQp-aaHHxehmT2HgdVxPv12CWBghBl",
-  Aquilo: "https://drive.google.com/uc?export=view&id=1tTYtliSC3y_5Le0S1iZmVEsQz7_CRSRj"
+  Nauvis: "https://dl.dropboxusercontent.com/scl/fi/ca5vjx2nc8nffnjsofhg6/nauvis.png?rlkey=sndw3nunhahfkfe69qxt6dakl",
+  Vulcanus: "https://dl.dropboxusercontent.com/scl/fi/dc2d15kqt6br4jgfr1uk3/vulcanus.png?rlkey=fhkrjd7x6ltjr4tcspx40w22x",
+  Gleba: "https://dl.dropboxusercontent.com/scl/fi/60kfazmrjj49psssnf7yk/gleba.png?rlkey=90c599jzdrxze1a2ccalqkzsc",
+  Fulgora: "https://dl.dropboxusercontent.com/scl/fi/p0jp4m2u1p18wjt5672i9/fulgora.png?rlkey=e6yat9d7z41k3cqr76ftoqjzo",
+  Aquilo: "https://dl.dropboxusercontent.com/scl/fi/woyz1jpeub931mexis4c9/aquilo.png?rlkey=3f8yhsotj53mgop3f9st670oc"
 };
